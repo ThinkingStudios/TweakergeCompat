5803fcd09872dbe1c14f666a42e1be2ba05688b9
TexTrue <3140846162@qq.com>
2024-08-26 19:45:37 +0800

    clean

--- src/main/java/org/thinkingstudio/tweakerge/compat/TweakergeCompatMod.java
+++ src/main/java/org/thinkingstudio/tweakerge/compat/TweakergeCompatMod.java
@@ -1,12 +1,14 @@
 package org.thinkingstudio.tweakerge.compat;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(TweakergeCompatMod.MOD_ID)
 public final class TweakergeCompatMod {
     public static final String MOD_ID = "tweakerge_compat";
 
     public TweakergeCompatMod() {
-
+        ModLoadingContext.get().registerDisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true);
     }
 }
